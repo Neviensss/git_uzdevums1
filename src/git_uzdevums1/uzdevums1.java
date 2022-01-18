74330c2ff85c28609f39b8a6fc55831ba46314d8
Leimanis <niks.leimanis3@gmail.com>
2022-01-18 12:17:19 +0200

    Pievienota kriteriju ievade

--- src/git_uzdevums1/uzdevums1.java
+++ src/git_uzdevums1/uzdevums1.java
@@ -13,12 +13,22 @@ public class uzdevums1 {
 		System.out.print("Cik skolēnus vēlies ievadīt?: ");
 		daudzums = dati.nextInt();
 		String[] skoleni = new String[daudzums];
+		String[] kriteriji = new String[daudzums];
 		
 		for(int i=0;i<skoleni.length;i++){
 			System.out.print("Ievadi skolēnu vārdus: ");
 			skoleni[i] = dati.next();
 		}
 		
+		for(int i=0;i<daudzums;i++){
+			System.out.print("Ievadi vērtēšanas kritērijus: ");
+			 kriteriji[i] = dati.next();
+		}
+		int skaititajs= 0;
+		for(int i=0;i<daudzums;i++){
+			System.out.print("Ievadi % svarīgumu kritērijam "+kriteriji[skaititajs]+": \n");
+			skaititajs++;
+		}
 		
 		
 		dati.close();
