c91480f54e470e85680ca32c84ff9d91bdb9232f
Leimanis <niks.leimanis3@gmail.com>
2022-01-18 11:59:03 +0200

    Pievienota vārdu ievade

--- src/git_uzdevums1/uzdevums1.java
+++ src/git_uzdevums1/uzdevums1.java
@@ -1,9 +1,27 @@
 package git_uzdevums1;
 
+import java.util.Scanner;
+
 public class uzdevums1 {
 
 	public static void main(String[] args) {
-
+		Scanner dati = new Scanner(System.in);
+		int[]atzime;
+		int[]procenti;
+		int daudzums;
+		
+		System.out.print("Cik skolēnus vēlies ievadīt?: ");
+		daudzums = dati.nextInt();
+		String[] skoleni = new String[daudzums];
+		
+		for(int i=0;i<skoleni.length;i++){
+			System.out.print("Ievadi skolēnu vārdus: ");
+			skoleni[i] = dati.next();
+		}
+		
+		
+		
+		dati.close();
 	}
 
 }
