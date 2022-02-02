799cba0ceccc0bfe25965b962a1e191bd4827426
Leimanis <niks.leimanis3@gmail.com>
2022-02-02 13:10:44 +0200

    Izveidota programma

--- src/git_uzdevums1/uzdevums1.java
+++ src/git_uzdevums1/uzdevums1.java
@@ -1,37 +1,75 @@
 package git_uzdevums1;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class uzdevums1 {
-
 	public static void main(String[] args) {
-		Scanner dati = new Scanner(System.in);
-		int[]atzime;
-		int[]procenti;
-		int daudzums;
+		int studSk, kritSk;
+		Scanner scan = new Scanner(System.in);
+		DecimalFormat df = new DecimalFormat("0.#");
+		     
+		do {
+			System.out.println("Cik studentiem aprēķināsi gala vērtējumu?");
+			studSk = scan.nextInt();
+		}while(studSk<1);
+		String[] studenti = new String[studSk];
 		
-		System.out.print("Cik skolēnus vēlies ievadīt?: ");
-		daudzums = dati.nextInt();
-		String[] skoleni = new String[daudzums];
-		String[] kriteriji = new String[daudzums];
+		do {
+			System.out.println("Kāds būs kritēriju skaits?");
+			kritSk = scan.nextInt();
+		}while(kritSk<1);
+		String[] kriteriji = new String[kritSk];
+		int[] kriterijaSvars = new int[kritSk];
+		int[][] kriterijaVertejums = new int[studSk][kritSk];
+		double[] semestraVertejums = new double[studSk];
 		
-		for(int i=0;i<skoleni.length;i++){
-			System.out.print("Ievadi skolēnu vārdus: ");
-			skoleni[i] = dati.next();
+		//Ievada studentu vārdus, uzvārdus
+		for(int i=0; i<studenti.length; i++) {
+			System.out.println("Ievadi "+(i+1)+". studentu");
+			studenti[i] = scan.next();
 		}
 		
-		for(int i=0;i<daudzums;i++){
-			System.out.print("Ievadi vērtēšanas kritērijus: ");
-			 kriteriji[i] = dati.next();
-		}
-		int skaititajs= 0;
-		for(int i=0;i<daudzums;i++){
-			System.out.print("Ievadi % svarīgumu kritērijam "+kriteriji[skaititajs]+": \n");
-			skaititajs++;
+		//Definē kritērijus
+		int maxSvars = 100;
+		for(int i=0; i<kriteriji.length; i++) {
+			System.out.println("Ievadi "+(i+1)+". kritēriju");
+			kriteriji[i] = scan.next();
+			//Norāda katra kritērija svaru
+			do {
+				System.out.println("Ievadi "+(i+1)+". kritērija svaru");
+				kriterijaSvars[i] = scan.nextInt();
+			}while(kriterijaSvars[i]>maxSvars || 
+					kriterijaSvars[i]<1 || 
+					(kriterijaSvars[0]==100 && kritSk > 1));
+			maxSvars -= kriterijaSvars[i];
 		}
 		
+		//Norāda vērtējumu kādu ieguvis katrs students par katru kritēriju
+		for(int i=0; i<kriterijaVertejums.length; i++) {
+			for(int j=0; j<kriterijaVertejums[i].length; j++) {
+				do {
+					System.out.println("Ievadi "+studenti[i]+" vērtējumu par kritēriju "+kriteriji[j]);
+					kriterijaVertejums[i][j] = scan.nextInt();
+				}while(kriterijaVertejums[i][j]<0 || kriterijaVertejums[i][j]>10);
+			}
+		}
 		
-		dati.close();
+		double rezultats;
+		for(int i=0; i<studenti.length; i++) {
+			rezultats=0;
+			for(int j=0; j<kriteriji.length; j++) {
+				rezultats += ((double) kriterijaSvars[j]/100)*kriterijaVertejums[i][j];
+			}
+			semestraVertejums[i] = rezultats;
+		}
+		
+		for(int i=0; i<studenti.length; i++) {	
+			for(int j=0; j<kriteriji.length; j++) {
+				System.out.println("Studenta "+studenti[i]+" vērtējums par kritēriju "+kriteriji[j]+" ir "+kriterijaVertejums[i][j]+", kura svars ir "+kriterijaSvars[j]);
+			}
+			System.out.println("Semestra vērtējums ir "+df.format(semestraVertejums[i])+"\n");
+		}
+		scan.close();
 	}
-
 }
